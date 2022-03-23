55345431d28862d439a9c48339279006067cdb62
Bùi Tùng LNU <bvtung17@gmail.com>
2022-03-23 16:05:28 +0700

    update DI

--- src/Services/DailyTask/DailyTask.API/Program.cs
+++ src/Services/DailyTask/DailyTask.API/Program.cs
@@ -1,19 +1,9 @@
 using System.Reflection;
 using AutoMapper;
-using DailyTask.Application.Contracts.Interfaces.IServices;
-using DailyTask.Application.Contracts.Interfaces.Persistence;
-using DailyTask.Application.Contracts.Services;
-using DailyTask.Application.Features.DailyTasks.Handlers;
+using DailyTask.Application;
 using DailyTask.Application.Mappings;
-using DailyTask.Application.PipelineBehaviours;
-using DailyTask.Application.Validations.Commands;
 using DailyTask.Infrastructure;
-using DailyTask.Infrastructure.Persistence;
-using DailyTask.Infrastructure.Repositories;
-using FluentValidation;
-using FluentValidation.AspNetCore;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,10 +18,10 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddDbContext<DailyDbContext>(option =>
-{
-    option.UseNpgsql(builder.Configuration.GetConnectionString("DailyTaskContext"));
-});
+//Add DependencyInjection
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
 // Auto Mapper Configurations
 var mapperConfig = new MapperConfiguration(mc =>
 {
@@ -40,30 +30,8 @@ var mapperConfig = new MapperConfiguration(mc =>
 
 IMapper mapper = mapperConfig.CreateMapper();
 builder.Services.AddSingleton(mapper);
-//DI
-builder.Services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
-builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
-builder.Services.AddTransient<IAuthenticationService, AuthenticationService >();
-builder.Services.AddTransient<IUserService, UserService>();
-builder.Services.AddTransient<ITaskDailyService, TaskDailyService>();
-//MediatR
-//builder.Services.AddScoped<IGetTaskDailyByIdQueryHandler, GetTaskDailyByIdQueryHandler>();
 
-builder.Services.AddMediatR(typeof(GetTaskDailyByIdQueryHandler).GetTypeInfo().Assembly);
-builder.Services.AddMediatR(typeof(GetAllTaskDailyQueryHandler).GetTypeInfo().Assembly);
-builder.Services.AddMediatR(typeof(CreateTaskDailyCommandHandler).GetTypeInfo().Assembly);
-builder.Services.AddMediatR(typeof(DeleteTaskDailyCommandHandler).GetTypeInfo().Assembly);
-builder.Services.AddMediatR(typeof(UpdateTaskDailyCommandHandler).GetTypeInfo().Assembly);
 
-builder.Services.AddMediatR(typeof(GetUserByIdQueryHandler).GetTypeInfo().Assembly);
-builder.Services.AddMediatR(typeof(GetAllUserQueryHandler).GetTypeInfo().Assembly);
-builder.Services.AddMediatR(typeof(CreateUserCommandHandler).GetTypeInfo().Assembly);
-builder.Services.AddMediatR(typeof(DeleteUserCommandHandler).GetTypeInfo().Assembly);
-builder.Services.AddMediatR(typeof(UpdateUserCommandHandler).GetTypeInfo().Assembly);
-//Validator
-
-builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-builder.Services.AddValidatorsFromAssembly(typeof(CreateTaskDailyValidator).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ src/Services/DailyTask/DailyTask.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using DailyTask.Application.Contracts.Interfaces.Persistence;
+using DailyTask.Infrastructure.Persistence;
+using DailyTask.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyTask.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<DailyDbContext>(option =>
+            {
+                option.UseNpgsql(configuration.GetConnectionString("DailyTaskContext"));
+            });
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+            return services;
+        }
+    }
+}
