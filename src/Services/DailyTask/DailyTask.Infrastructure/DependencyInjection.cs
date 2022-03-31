fc93b3a712ca2f7206c2a95551792eb5c93a5111
Bùi Tùng DevFunny <bvtung17@devfunny.onmicrosoft.com>
2022-03-31 10:30:36 +0700

    Caching with Hangfire PostgreSQL

--- src/Services/DailyTask/DailyTask.Infrastructure/DependencyInjection.cs
+++ src/Services/DailyTask/DailyTask.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,10 @@
 ﻿using DailyTask.Application.Contracts.Interfaces.Persistence;
 using DailyTask.Domain.Entities;
+using DailyTask.Infrastructure.Cache;
 using DailyTask.Infrastructure.Persistence;
 using DailyTask.Infrastructure.Repositories;
+using Hangfire;
+using Hangfire.PostgreSql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,7 +23,26 @@ namespace DailyTask.Infrastructure
             services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
             services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
             //services.Decorate<IAsyncRepository<TaskDaily>, RepositoryBase<TaskDaily>>();
+
+            services.Configure<CacheConfiguration>(configuration.GetSection("CacheConfiguration"));
             services.AddMemoryCache();
+            services.AddTransient<MemoryCacheService>();
+            services.AddTransient<RedisCacheService>();
+            services.AddTransient<Func<CacheTech, ICacheService>>(serviceProvider => key =>
+            {
+                switch (key)
+                {
+                    case CacheTech.Memory:
+                        return serviceProvider.GetService<MemoryCacheService>();
+                    case CacheTech.Redis:
+                        return serviceProvider.GetService<RedisCacheService>();
+                    default:
+                        return serviceProvider.GetService<MemoryCacheService>();
+                }
+            });
+            services.AddHangfire(config =>
+                config.UsePostgreSqlStorage(configuration.GetConnectionString("DailyTaskContext")));
+            services.AddHangfireServer();
             return services;
         }
     }
